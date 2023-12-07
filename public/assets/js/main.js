37bf0c3c73e92c54931d1e36738f9926a6331970
wedsney <wedsney22@gmail>
2023-12-07 14:29:43 -0300

    ajuste  de código js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,10 @@
-
 var menuOpen = false;
 
 function toggleSideMenu() {
     var sideMenu = document.getElementById('sideMenu');
     if (sideMenu.style.left === '0px') {
         sideMenu.style.left = '-250px';
-       
+
 
     } else {
         sideMenu.style.left = '0px';
@@ -16,31 +15,43 @@ function toggleSideMenu() {
 function closeSideMenu() {
     var sideMenu = document.getElementById('sideMenu');
     sideMenu.style.left = '-250px';
+    
 }
 
-function loadContent(url) {
-    var contentArea = document.getElementById('contentArea');
+window.addEventListener('dblclick', closeSideMenu);
 
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao carregar o conteúdo da URL: ' + response.status);
-            }
-            return response.text();
-        })
-        .then(data => {
-            contentArea.innerHTML = data;
-            closeSideMenu();
-        })
-        .catch(error => {
-            console.error(error);
-        });
+
+function loadContent(content) {
+    var contentArea = document.getElementById('contentArea');
+   
+    if (content === 'fichas')
+        contentArea.innerHTML = `<div class="fichas-area">
+    <h1>Suas Fichas</h1>
+    <div>
+        <p>Crie sua nova ficha</p>
+        <button onclick="abreFichaModal()">+</button>
+    </div>
+</div>`;
+    else if (content === 'notes')
+        contentArea.innerHTML = `<section>
+<div class="anotacoes" id="anotacoes">
+    <label for="anotacoes"> Anotações</label>
+    <textarea name="anotacoes" id="" cols="30" rows="10">
+
+    </textarea>
+</div>
+</section>`;
+    else if (content === 'maps')
+    contentArea.innerHTML = `<img style="width:50%;" src="../imagens/bokislad 2.0 (1).jpg">`
+
+
 }
 
 
 function abreFichaModal() {
     var modal = document.getElementById('modal');
     modal.style.display = 'block';
+    closeSideMenu() 
 }
 
 function fechaFichaModal() {
@@ -49,37 +60,10 @@ function fechaFichaModal() {
 }
 
 // Fecha o modal quando o usuário clica fora do conteúdo do modal
-window.onclick = function(event) {
+window.onclick = function (event) {
+    
     var modal = document.getElementById('modal');
     if (event.target == modal) {
         modal.style.display = 'none';
     }
 }
-
-function salvaFicha() {
-    // Add an event listener to the form submission
-    document.querySelector('form').addEventListener('submit', function (event) {
-      event.preventDefault(); // Prevent the default form submission
-       
-      // Here, you can gather form data and send it to your server or perform other actions
-      const formData = new FormData(this);
-  
-      // Example: Send form data to the server using fetch
-      fetch('/submit', {
-        method: 'POST',
-        body: formData,
-      })
-        .then((response) => {
-          if (response.ok) {
-            // The form data was successfully sent to the server
-            console.log('Form data sent successfully.');
-          } else {
-            // Handle errors
-            console.error('Error sending form data to the server.');
-          }
-        })
-        .catch((error) => {
-          console.error('An error occurred:', error);
-        });
-    });
-  }
